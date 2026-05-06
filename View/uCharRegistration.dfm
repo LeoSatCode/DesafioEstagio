cee5553b911c51ae00b3f990f057f2a81c1821eb
Dev MV <devmv@domtec.svr>
2026-05-06 14:56:11 -0300

    feat: layout da tela de cadastro e validacao de campos

--- View/uCharRegistration.dfm
+++ View/uCharRegistration.dfm
@@ -1,16 +1,153 @@
 object frmCharRegistration: TfrmCharRegistration
   Left = 0
   Top = 0
-  Caption = 'frmCharRegistration'
-  ClientHeight = 201
-  ClientWidth = 447
-  Color = clBtnFace
+  Caption = 'Cadastro de Personagem'
+  ClientHeight = 460
+  ClientWidth = 520
+  Color = clWhite
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
   Font.Style = []
-  OldCreateOrder = False
+  OldCreateOrder = True
+  Position = poScreenCenter
   PixelsPerInch = 96
-  TextHeight = 13
+  TextHeight = 15
+  object pnlHeader: TPanel
+    Left = 0
+    Top = 0
+    Width = 520
+    Height = 70
+    Align = alTop
+    BevelOuter = bvNone
+    ParentBackground = False
+    TabOrder = 0
+    object lblTitle: TLabel
+      Left = 20
+      Top = 25
+      Width = 155
+      Height = 25
+      Caption = 'Novo Personagem'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -18
+      Font.Name = 'Segoe UI'
+      Font.Style = [fsBold]
+      ParentFont = False
+    end
+  end
+  object pnlContent: TPanel
+    Left = 0
+    Top = 70
+    Width = 520
+    Height = 322
+    Align = alClient
+    BevelOuter = bvNone
+    TabOrder = 1
+    object lblName: TLabel
+      Left = 20
+      Top = 20
+      Width = 33
+      Height = 15
+      Caption = 'Nome'
+    end
+    object lblFranchise: TLabel
+      Left = 20
+      Top = 75
+      Width = 46
+      Height = 15
+      Caption = 'Franquia'
+    end
+    object lblActor: TLabel
+      Left = 260
+      Top = 75
+      Width = 52
+      Height = 15
+      Caption = 'Ator/Atriz'
+    end
+    object lblMedia: TLabel
+      Left = 20
+      Top = 130
+      Width = 72
+      Height = 15
+      Caption = 'Tipo de M'#237'dia'
+    end
+    object lblDescription: TLabel
+      Left = 20
+      Top = 185
+      Width = 51
+      Height = 15
+      Caption = 'Descri'#231#227'o'
+    end
+    object edtName: TEdit
+      Left = 20
+      Top = 40
+      Width = 460
+      Height = 23
+      TabOrder = 0
+    end
+    object cmbFranchise: TComboBox
+      Left = 20
+      Top = 95
+      Width = 220
+      Height = 23
+      TabOrder = 1
+    end
+    object cmbActor: TComboBox
+      Left = 260
+      Top = 95
+      Width = 220
+      Height = 23
+      TabOrder = 2
+    end
+    object cmbMedia: TComboBox
+      Left = 20
+      Top = 150
+      Width = 220
+      Height = 23
+      Style = csDropDownList
+      TabOrder = 3
+      Items.Strings = (
+        'Filme'
+        'S'#195#169'rie'
+        'Anime'
+        'Jogo'
+        'Outro')
+    end
+    object memDescription: TMemo
+      Left = 20
+      Top = 205
+      Width = 460
+      Height = 92
+      TabOrder = 4
+    end
+  end
+  object pnlFooter: TPanel
+    Left = 0
+    Top = 392
+    Width = 520
+    Height = 68
+    Align = alBottom
+    BevelOuter = bvNone
+    TabOrder = 2
+    object btnSave: TPngBitBtn
+      Left = 300
+      Top = 20
+      Width = 90
+      Height = 32
+      Caption = 'Salvar'
+      Default = True
+      TabOrder = 0
+    end
+    object btnCancel: TPngBitBtn
+      Left = 400
+      Top = 20
+      Width = 90
+      Height = 32
+      Cancel = True
+      Caption = 'Cancelar'
+      TabOrder = 1
+    end
+  end
 end
--- View/uCharRegistration.pas
+++ View/uCharRegistration.pas
@@ -3,15 +3,39 @@ unit uCharRegistration;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Buttons, PngBitBtn, cCharacter, uDTMConexao;
 
 type
   TfrmCharRegistration = class(TForm)
+    pnlHeader: TPanel;
+    lblTitle: TLabel;
+    pnlContent: TPanel;
+    lblName: TLabel;
+    lblFranchise: TLabel;
+    lblActor: TLabel;
+    lblMedia: TLabel;
+    lblDescription: TLabel;
+    edtName: TEdit;
+    cmbFranchise: TComboBox;
+    cmbActor: TComboBox;
+    cmbMedia: TComboBox;
+    memDescription: TMemo;
+    pnlFooter: TPanel;
+    btnSave: TPngBitBtn;
+    btnCancel: TPngBitBtn;
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnSaveClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
-    { Private declarations }
+    FCharacter: TCharacter;
+    procedure LoadLists;
+    procedure ObjectToScreen;
+    procedure ScreenToObject;
   public
-    { Public declarations }
+    // O formulário recebe o objeto via Injeção de Dependência
+    property Character: TCharacter read FCharacter write FCharacter;
   end;
 
 var
@@ -20,5 +44,90 @@ var
 implementation
 
 {$R *.dfm}
+
+uses
+  FireDAC.Comp.Client;
+
+procedure TfrmCharRegistration.FormShow(Sender: TObject);
+begin
+  LoadLists; // Carrega o que já existe no banco nos ComboBoxes
+  ObjectToScreen; // Se for edição, preenche a tela
+  edtName.SetFocus;
+end;
+
+procedure TfrmCharRegistration.LoadLists;
+var
+  Qry: TFDQuery;
+begin
+  Qry := TFDQuery.Create(nil);
+  try
+    Qry.Connection := dtmConnection.ConnectionDB;
+
+    // Busca Franquias existentes
+    cmbFranchise.Items.Clear;
+    Qry.Open('SELECT nome FROM Franquias ORDER BY nome');
+    while not Qry.Eof do
+    begin
+      cmbFranchise.Items.Add(Qry.FieldByName('nome').AsString);
+      Qry.Next;
+    end;
+
+    // Busca Atores existentes
+    cmbActor.Items.Clear;
+    Qry.Open('SELECT nome FROM Atores ORDER BY nome');
+    while not Qry.Eof do
+    begin
+      cmbActor.Items.Add(Qry.FieldByName('nome').AsString);
+      Qry.Next;
+    end;
+  finally
+    Qry.Free;
+  end;
+end;
+
+procedure TfrmCharRegistration.ObjectToScreen;
+begin
+  if Assigned(FCharacter) then
+  begin
+    edtName.Text := FCharacter.Name;
+    cmbFranchise.Text := FCharacter.Franchise;
+    cmbActor.Text := FCharacter.ActorOrActress;
+    cmbMedia.Text := FCharacter.MediaType;
+    memDescription.Text := FCharacter.Description;
+
+    if FCharacter.Id > 0 then
+      lblTitle.Caption := 'Editar Personagem'
+    else
+      lblTitle.Caption := 'Novo Personagem';
+  end;
+end;
+
+procedure TfrmCharRegistration.ScreenToObject;
+begin
+  FCharacter.Name := edtName.Text;
+  FCharacter.Franchise := cmbFranchise.Text;
+  FCharacter.ActorOrActress := cmbActor.Text;
+  FCharacter.MediaType := cmbMedia.Text;
+  FCharacter.Description := memDescription.Text;
+end;
+
+procedure TfrmCharRegistration.btnSaveClick(Sender: TObject);
+begin
+  // Validação simples usando o método Trim clássico do SysUtils
+  if Trim(edtName.Text) = '' then
+  begin
+    ShowMessage('O nome do personagem é obrigatório!');
+    edtName.SetFocus;
+    Exit;
+  end;
+
+  ScreenToObject;
+  ModalResult := mrOk; // Fecha a tela avisando que pode salvar
+end;
+
+procedure TfrmCharRegistration.btnCancelClick(Sender: TObject);
+begin
+  ModalResult := mrCancel;
+end;
 
 end.
